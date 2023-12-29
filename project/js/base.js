7b600efd2d2d0cc3da327e50dd39bee988dd4c0e
kyum-q <109158497+kyum-q@users.noreply.github.com>
2023-12-30 02:18:24 +0900

    refector(테이블 동적 생성) : 테트리스 틀(테이블)을 동적 생성하는 코드로 구현

--- project/js/base.js
+++ project/js/base.js
@@ -15,30 +15,32 @@ function init(w, h) {
     // 10x10 2차원 배열 만들기
     // 블록 배열 생성 및 초기화
     blockArray = new Array(HEIGHT);
-    for(let i=0; i<HEIGHT; i++) {
+    for (let i = 0; i < HEIGHT; i++) {
         blockArray[i] = new Array(WIDTH);
-        for(let j=0; j<WIDTH; j++)
+        for (let j = 0; j < WIDTH; j++)
             blockArray[i][j] = null; // 테스트 시에 3으로 초기화
     }
+}
 
-    // 생성된 배열 출력하여 테스트
-    /*
-    var str="";
-    for(let i=0; i<10; i++) {
-        for(let j=0; j<10; j++)
-            str += blockArray[i][j] + " ";
-        str += "\n";
+function setTetrisTable() {
+    const table = document.getElementById("tetris_table");
+    for (let i = 0; i < HEIGHT; i++) {
+        const tr = document.createElement('tr');
+        for (let j = 0; j < WIDTH; j++) {
+            const td = document.createElement('td');
+            td.className = 'tetris_td';
+            tr.append(td);
+        }
+        table.append(tr);
     }
-
-    alert(str);
-    */
 }
 
 window.onload = function () {
-    init(10, 10);
-    tds = document.getElementsByTagName("td");
-    tds[blockLoc].style.backgroundColor = "skyblue";
+    init(7, 5);
+    setTetrisTable();
 
+    tds = document.getElementsByClassName("tetris_td");
+    tds[blockLoc].style.backgroundColor = "skyblue";
 
     // timer set
     timerID = setInterval("moveDown()",300);
